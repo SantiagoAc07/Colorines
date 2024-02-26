bf2c0a5e9ec7e39192235645e259fc09bdbd189d
Santiago Alzate C <141456661+SantiagoAc07@users.noreply.github.com>
2024-02-25 20:38:01 -0500

    Script POO

--- Assets/Scripts/Player/Movement/Imovement.cs
+++ Assets/Scripts/Player/Movement/Imovement.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Imovement : MonoBehaviour
+namespace Player.Movement
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IMovement
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Move(Vector2 direction);
     }
 }
--- Assets/Scripts/Player/Movement/NewMonoBehaviour.cs
+++ Assets/Scripts/Player/Movement/NewMonoBehaviour.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class Floormovement:
 {
     // Use this for initialization
     void Start()
